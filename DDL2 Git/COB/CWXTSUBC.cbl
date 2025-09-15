34234c9c79e1f0edbdba310175060d45e6c5e838
Dakota Locklear <dakota_locklear@bmc.com>
2025-09-15 15:54:31 -0400

    Update Date / Time

--- DDL2 Git/COB/CWXTSUBC.cbl
+++ DDL2 Git/COB/CWXTSUBC.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
       *  XPEDITER/TSO TRAINING SESSIONS.                               *
-      *                                                                *
+      *  9/15/2025                                                     *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
